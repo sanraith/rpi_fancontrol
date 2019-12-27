40a0ca59518c9a5b5258877383d30b2dfc34d997
Soma Zsják <zsjaks@gmail.com>
2019-12-27 22:17:10 +0100

    Updated parameters and first on-off condition.

--- rpi_fancontrol/__main__.py
+++ rpi_fancontrol/__main__.py
@@ -4,8 +4,8 @@ from time import sleep
 
 temp_max = 70
 temp_min = 40
-test_mode = True
-fan_pin = 17
+test_mode = False
+fan_pin = 26
 
 
 def main():
@@ -14,9 +14,9 @@ def main():
     while True:
         temp = reader.read_cpu_temperature()
         print(temp)
-        if (temp > temp_max and not fan.powered):
+        if (temp > temp_max and (fan.powered is None or not fan.powered)):
             fan.power(True)
-        elif (temp < temp_min and fan.powered):
+        elif (temp < temp_min and (fan.powered is None or fan.powered)):
             fan.power(False)
         sleep(1)
 
--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(name='rpi_fancontrol',
       version='1.0',
       description='Turn the fan of my Raspberry Pi on-off based on its CPU temperature.',
       author='Soma Zsják',
-      author_email='soma.zsjak@gmail.com',
+      author_email='zsjaks@gmail.com',
       url='https://github.com/sanraith/rpi_fancontrol',
       packages=['rpi_fancontrol'],)
